d4a0543aeb8630f82da11cf06ca4881542a54a20
Mert Bölükbaşı <mert54bolukbasi@gmail.com>
2025-05-02 12:30:16 +0300

    düzeldi inş

--- src/navbar2.jsx
+++ src/navbar2.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar2.css";
-
+import "./navbar2.css";
 
 const Navbar2 = () => {
   const [menuOpen, setMenuOpen] = useState(false);
