ae60537a8ce54711d0ef88f21bcf07a650cdc8c3
Mert Bölükbaşı <mert54bolukbasi@gmail.com>
2025-05-12 15:18:49 +0300

    footer bug fixlendi

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,30 +4,29 @@ import './Footer.css'; // CSS dosyasını unutma!
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <div className="footer-section about">
-          <h2>Ruvido Tekstil</h2>
-          
-        </div>
+      <div className="footer-inner">
+        <div className="footer-content">
+          <div className="footer-section">
+            <h2>Ruvido Tekstil</h2>
+          </div>
 
-        <div className="footer-section links">
-          <h3>Sosyal Medya</h3>
-          <ul>
-            <li><a href="https://www.instagram.com/ruvido.tr">Instagram</a></li>
-          </ul>
-        </div>
+          <div className="footer-section">
+            <h3>Sosyal Medya</h3>
+            <p><a href="https://www.instagram.com/ruvido.tr">Instagram</a></p>
+          </div>
 
-        <div className="footer-section contact">
-          <h3>İletişim</h3>
-          <p>ruvidotekstil@gmail.com</p>
-          <p>+90 545 615 18 75</p>
-          <p>+90 545 833 49 25</p>
-          <p>+90 538 895 96 61</p>
+          <div className="footer-section">
+            <h3>İletişim</h3>
+            <p>ruvidotekstil@gmail.com</p>
+            <p>+90 545 615 18 75</p>
+            <p>+90 545 833 49 25</p>
+            <p>+90 538 895 96 61</p>
+          </div>
         </div>
-      </div>
-
-      <div className="footer-bottom">
-        © 2025 Ruvido Tekstil. Tüm Hakları Saklıdır.
+        
+        <div className="footer-bottom">
+          <p>© 2025 Ruvido Tekstil. Tüm Hakları Saklıdır.</p>
+        </div>
       </div>
     </footer>
   );
